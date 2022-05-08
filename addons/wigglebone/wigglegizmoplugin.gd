0ba007a3781ad04a28f7df309ee28ed0880778ca
detomon <simon@monoxid.ch>
2022-05-08 21:47:58 +0200

    Cleanup

--- addons/wigglebone/wigglegizmoplugin.gd
+++ addons/wigglebone/wigglegizmoplugin.gd
@@ -43,11 +43,11 @@ func set_handle(gizmo: EditorSpatialGizmo, index: int, camera: Camera, point: Ve
 		handle_init_position = handle_position
 		handle_dragging = true
 
-	bone.set_const_force(handle_position - handle_init_position)
+	bone.const_force = handle_position - handle_init_position
 
 func commit_handle(gizmo: EditorSpatialGizmo, index: int, restore, cancel: bool = false) -> void:
 	var bone: WiggleBone = gizmo.get_spatial_node()
-	bone.set_const_force(Vector3.ZERO)
+	bone.const_force = Vector3.ZERO
 
 	handle_init_position = Vector3.ZERO
 	handle_position = Vector3.ZERO
@@ -55,22 +55,23 @@ func commit_handle(gizmo: EditorSpatialGizmo, index: int, restore, cancel: bool 
 
 func redraw(gizmo: EditorSpatialGizmo) -> void:
 	var wigglebone: WiggleBone = gizmo.get_spatial_node()
-	var properties: WiggleProperties = wigglebone.properties
+	var properties: = wigglebone.properties
 
 	gizmo.clear()
 
-	if properties and wigglebone.show_gizmo:
+	if properties:
 		match properties.mode:
 			WiggleProperties.Mode.ROTATION:
 				var length: = properties.mass_center.length()
-				var scale_x: = sin(deg2rad(properties.max_degrees))
-				var scale_y: = cos(deg2rad(properties.max_degrees))
+				var angle: = deg2rad(properties.max_degrees)
+				var scale_x: = sin(angle)
+				var scale_y: = cos(angle)
 				var scale: = Vector3(scale_x, scale_y, scale_x)
 
 				var bone_look_at: = WiggleBone.create_bone_look_at(properties.mass_center, Vector3.RIGHT)
-				var transform: = bone_look_at * Basis().scaled(scale * length * 0.75)
+				var transform: = Transform(bone_look_at.scaled(scale * length * 0.75), Vector3.ZERO)
 
-				var lines: = transform_points(cone_lines, transform)
+				var lines: PoolVector3Array = transform.xform(cone_lines)
 				lines.append(Vector3.ZERO)
 				lines.append(properties.mass_center)
 				gizmo.add_lines(lines, get_material("main", gizmo), false)
@@ -78,9 +79,9 @@ func redraw(gizmo: EditorSpatialGizmo) -> void:
 			WiggleProperties.Mode.DISLOCATION:
 				var max_distance: = properties.max_distance
 				var scale: = Vector3.ONE * max_distance
-				var transform: = Basis().scaled(scale)
+				var transform: = Transform().scaled(scale)
 
-				var lines: = transform_points(sphere_lines, transform)
+				var lines: PoolVector3Array = transform.xform(sphere_lines)
 				gizmo.add_lines(lines, get_material("main", gizmo), true)
 
 		var handle: = get_handle_position(properties)
@@ -99,20 +100,23 @@ static func get_handle_position(properties: WiggleProperties) -> Vector3:
 
 static func generate_cone_lines() -> PoolVector3Array:
 	var count: = 32
+	var points: = PoolVector3Array()
+
+	for i in count:
+		points.append(Vector3(1, 1, 0).rotated(Vector3.UP, i * TAU / count))
+
 	var lines: = PoolVector3Array()
-	var prev_point: = Vector3(1, 1, 0)
 
-	for i in range(1, count + 1):
-		var point: = Vector3(1, 1, 0).rotated(Vector3.UP, i * TAU / count)
-
-		lines.append(prev_point)
-		lines.append(point)
+	for i in len(points) - 1:
+		lines.append(points[i])
+		lines.append(points[i + 1])
 
 		if i % 8 == 0:
-			lines.append(Vector3(0, 0, 0))
-			lines.append(point)
+			lines.append(Vector3.ZERO)
+			lines.append(points[i])
 
-		prev_point = point
+	lines.append(points[0])
+	lines.append(points[len(points) - 1])
 
 	return lines
 
@@ -121,28 +125,23 @@ static func generate_sphere_lines() -> PoolVector3Array:
 	var points: = PoolVector3Array()
 
 	for i in count:
-		points.append(Vector3(1, 0, 0).rotated(Vector3.UP, i * TAU / count))
+		points.append(Vector3.RIGHT.rotated(Vector3.UP, i * TAU / count))
 
 	var lines: = PoolVector3Array()
 	var rotations: = [
-		Basis(),
-		Basis().rotated(Vector3.RIGHT, PI * 0.5),
-		Basis().rotated(Vector3.BACK, PI * 0.5),
+		Transform(),
+		Transform().rotated(Vector3.RIGHT, PI * 0.5),
+		Transform().rotated(Vector3.BACK, PI * 0.5),
 	]
 
 	for transform in rotations:
-		var new_points: = transform_points(points, transform)
+		var new_lines: PoolVector3Array = transform.xform(points)
 
-		for i in count:
-			lines.append(new_points[i])
-			lines.append(new_points[(i + 1) % count])
+		for i in len(new_lines) - 1:
+			lines.append(new_lines[i])
+			lines.append(new_lines[i + 1])
+
+		lines.append(new_lines[0])
+		lines.append(new_lines[len(new_lines) - 1])
 
 	return lines
-
-static func transform_points(points: PoolVector3Array, transform: Basis) -> PoolVector3Array:
-	var new_points: = points
-
-	for i in len(new_points):
-		new_points[i] = transform * new_points[i]
-
-	return new_points
--- addons/wigglebone/wiggleproperties.gd
+++ addons/wigglebone/wiggleproperties.gd
@@ -2,22 +2,19 @@ tool
 extends Resource
 class_name WiggleProperties
 
-const PROPERTY_VISIBLE: = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE
-const PROPERTY_HIDDEN: = PROPERTY_VISIBLE & ~PROPERTY_USAGE_EDITOR
-
 enum Mode {
 	ROTATION,
 	DISLOCATION,
 }
 
 # distance of mass center to bone root in pose space
-var mass_center: = Vector3(0.0, 0.5, 0.0) setget set_mass_center
+var mass_center: = Vector3.UP * 0.5 setget set_mass_center
 func set_mass_center(value: Vector3) -> void:
 	mass_center = value
 	emit_changed()
 
 # gravity pulling at mass center
-var gravity: = Vector3(0.0, -0.5, 0.0) setget set_gravity
+var gravity: = Vector3.DOWN * 0.5 setget set_gravity
 func set_gravity(value: Vector3) -> void:
 	gravity = value
 	emit_changed()
@@ -47,49 +44,57 @@ func set_max_distance(value: float) -> void:
 	max_distance = value
 	emit_changed()
 
-# maximum rotation relative to pose position
+# maximum rotation relstive to pose position
 var max_degrees: = 60.0 setget set_max_degrees
 func set_max_degrees(value: float) -> void:
 	max_degrees = value
 	emit_changed()
 
 func _get_property_list() -> Array:
-	return [{
+	var props: = [{
 		name = "mass_center",
 		type = TYPE_VECTOR3,
-		usage = PROPERTY_VISIBLE,
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
 	}, {
 		name = "gravity",
 		type = TYPE_VECTOR3,
-		usage = PROPERTY_VISIBLE,
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
 	}, {
 		name = "stiffness",
 		type = TYPE_REAL,
 		hint = PROPERTY_HINT_RANGE,
 		hint_string = "0,1",
-		usage = PROPERTY_VISIBLE,
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
 	}, {
 		name = "damping",
 		type = TYPE_REAL,
 		hint = PROPERTY_HINT_RANGE,
 		hint_string = "0.01,1",
-		usage = PROPERTY_VISIBLE,
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
 	}, {
 		name = "mode",
 		type = TYPE_INT,
 		hint = PROPERTY_HINT_ENUM,
 		hint_string = "Rotation,Dislocation",
-		usage = PROPERTY_VISIBLE,
-	}, {
-		name = "max_degrees",
-		type = TYPE_REAL,
-		hint = PROPERTY_HINT_RANGE,
-		hint_string = "0,90",
-		usage = PROPERTY_VISIBLE if mode == Mode.ROTATION else PROPERTY_HIDDEN,
-	}, {
-		name = "max_distance",
-		type = TYPE_REAL,
-		hint = PROPERTY_HINT_RANGE,
-		hint_string = "0,1,or_greater",
-		usage = PROPERTY_VISIBLE if mode == Mode.DISLOCATION else PROPERTY_HIDDEN,
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
 	}]
+
+	match mode:
+		Mode.ROTATION:
+			props.append({
+				name = "max_degrees",
+				type = TYPE_REAL,
+				hint = PROPERTY_HINT_RANGE,
+				hint_string = "0,90",
+				usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+			})
+		Mode.DISLOCATION:
+			props.append({
+				name = "max_distance",
+				type = TYPE_REAL,
+				hint = PROPERTY_HINT_RANGE,
+				hint_string = "0,1,or_greater",
+				usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+			})
+
+	return props
